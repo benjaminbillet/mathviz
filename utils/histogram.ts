d780fe14074d805a95ee05ba4fe3fc673703741e
benjaminbillet <contact@benjaminbillet.fr>
2020-12-01 15:06:07 +0100

    Refactored histogram extraction

--- utils/histogram.ts
+++ utils/histogram.ts
@@ -1,48 +1,44 @@
-import { PlotBuffer, ColorHistogram, SingleChannelHistogram } from './types';
+import { clamp } from './misc';
+import { ColorHistogram, SingleChannelHistogram } from './types';
 
-export const makeHistogram = (input: PlotBuffer, width: number, height: number, nbBins = 256) => {
+export const makeHistogram = (input: Float32Array, width: number, height: number, nbBins = 256): ColorHistogram => {
   const nbPixels = width * height;
-  const nbChannels = input.length / nbPixels;
+  const nbChannels = Math.trunc(input.length / nbPixels);
   const binSize = Math.trunc(256 / nbBins);
 
-  return new Array(nbChannels).fill(null).map(() => {
-    const histogram = new Uint32Array(nbBins).fill(0);
-    input.forEach((lum: number) => {
-      const bin = Math.trunc(lum / binSize);
-      histogram[bin]++;
-    });
-    return histogram;
-  });
-};
+  const histogram = new Array(nbChannels).fill(null).map(() => new Uint32Array(nbBins).fill(0));
 
-export const makeRegionHistogram = (input: PlotBuffer, width: number, height: number, x1: number, y1: number, x2: number, y2: number, nbBins = 256): ColorHistogram => {
-  const nbPixels = width * height;
-  const nbChannels = input.length / nbPixels;
-  const binSize = Math.trunc(256 / nbBins);
-
-  return new Array(nbChannels).fill(null).map(() => {
-    const histogram: SingleChannelHistogram = new Uint32Array(nbBins).fill(0);
-    for (let x = x1; x < x2; x++) {
-      for (let y = y1; y < y2; y++) {
-        let idx;
-        if (x >= width && y < height) {
-          idx = (y * width + (width - 1));
-        } else if (y >= height && x < width) {
-          idx = ((height - 1) * width + x);
-        } else if (y >= height && x >= width) {
-          idx = (height * width) - 1;
-        } else {
-          idx = (y * width + x);
-        }
-        const lum = input[idx];
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      const idx = (x + y * width) * 4;
+      for (let channel = 0; channel < nbChannels; channel++) {
+        const lum = input[idx + channel] * 255;
         const bin = Math.trunc(lum / binSize);
-        histogram[bin]++;
+        histogram[channel][bin]++;
       }
     }
-    return histogram;
-  });
+  }
+  return histogram;
 };
 
+export const makeRegionHistogram = (input: Float32Array, width: number, height: number, x1: number, y1: number, x2: number, y2: number, nbBins = 256): ColorHistogram => {
+  const nbPixels = width * height;
+  const nbChannels = Math.trunc(input.length / nbPixels);
+  const binSize = Math.trunc(256 / nbBins);
+
+  const histogram = new Array(nbChannels).fill(null).map(() => new Uint32Array(nbBins).fill(0));
+  for (let x = x1; x < x2; x++) {
+    for (let y = y1; y < y2; y++) {
+      const idx = (clamp(x, 0, width - 1) + clamp(y, 0, height - 1) * width) * 4;
+      for (let channel = 0; channel < nbChannels; channel++) {
+        const lum = input[idx + channel] * 255;
+        const bin = Math.trunc(lum / binSize);
+        histogram[channel][bin]++;
+      }
+    }
+  }
+  return histogram;
+};
 
 export const clipHistogram = (hist: ColorHistogram, clipLimit: number, redistribute = false) => {
   return hist.map((histChannel) => {
@@ -63,7 +59,6 @@ export const clipHistogram = (hist: ColorHistogram, clipLimit: number, redistrib
     // redistribute excess points in each bins
     const excessPerBin = Math.trunc(excess / histChannel.length); // average bin increment
     const binLimit = clipLimit - excessPerBin; // upper limit for bins
-    // console.log(histChannel, clipLimit, binLimit, excessPerBin, excess)
 
     histChannel = histChannel.map((value) => {
       if (value > binLimit) {
@@ -96,13 +91,12 @@ export const clipHistogram = (hist: ColorHistogram, clipLimit: number, redistrib
   });
 };
 
-export const scaleHistogram = (hist: ColorHistogram, scale: number) => {
+export const computeCumulativeHistogram = (hist: ColorHistogram, scale: number) => {
   return hist.map((histChannel) => {
     let sum = 0;
     return histChannel.map((value) => {
       sum += value;
       const newValue = sum * scale;
-      // console.log(newValue, sum, value, scale);
       if (newValue > 255) {
         return 255;
       }
@@ -114,13 +108,13 @@ export const scaleHistogram = (hist: ColorHistogram, scale: number) => {
 // https://webdocs.cs.ualberta.ca/~graphics/books/GraphicsGems/gemsiv/clahe.c
 // http://cas.xav.free.fr/Graphics%20Gems%204%20-%20Paul%20S.%20Heckbert.pdf
 // https://stackoverflow.com/questions/38346702/how-to-implement-contrast-limited-adaptive-histogram-equalization-using-clahe-in
-export const performClahe = (input: PlotBuffer, width: number, height: number, output: PlotBuffer, nbRegionsX = 16, nbRegionsY = 16, nbBins = 256, clipLimit = 128) => {
+export const performClahe = (input: Float32Array, width: number, height: number, output: Float32Array, nbRegionsX = 16, nbRegionsY = 16, nbBins = 256, clipLimit = 128) => {
   if (width % nbRegionsX !== 0 || height % nbRegionsY !== 0) {
     throw new Error('Invalid region size');
   }
 
-  const regionXSize = Math.ceil(width / nbRegionsX);
-  const regionYSize = Math.ceil(height / nbRegionsY);
+  const regionXSize = Math.trunc(width / nbRegionsX) + 1;
+  const regionYSize = Math.trunc(height / nbRegionsY) + 1;
   const regionArea = regionXSize * regionYSize;
   const regionXInvSize = 1 / regionXSize;
   const regionYInvSize = 1 / regionYSize;
@@ -139,29 +133,24 @@ export const performClahe = (input: PlotBuffer, width: number, height: number, o
       const x2 = x1 + regionXSize;
       const y2 = y1 + regionYSize;
 
-      // console.log(x, y);
       let hist = makeRegionHistogram(input, width, height, x1, y1, x2, y2, nbBins);
-      // console.log(hist, hist.reduce((sum, x) => sum + x, 0));
       hist = clipHistogram(hist, clipLimit, true);
-      // console.log(hist, hist.reduce((sum, x) => sum + x, 0));
-      hist = scaleHistogram(hist, 255 / regionArea);
-      // console.log(hist, hist.reduce((sum, x) => sum + x, 0));
-
+      hist = computeCumulativeHistogram(hist, 255 / regionArea);
       bwColumnHists.push(hist[0]); // single channel (gray)
     }
   }
 
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
-      const lum = Math.floor(input[y * width + x]);
+      const lum = Math.trunc(input[(y * width + x) * 4] * 255);
 
       const tx = x * regionXInvSize - 0.5;
       const ty = y * regionYInvSize - 0.5;
 
-      const xl = Math.max(Math.floor(tx), 0);
+      const xl = Math.max(Math.trunc(tx), 0);
       const xr = Math.min(xl+1, nbRegionsX - 1);
 
-      const yt = Math.max(Math.floor(ty), 0);
+      const yt = Math.max(Math.trunc(ty), 0);
       const yd = Math.min(yt+1, nbRegionsY - 1);
 
       const fx = tx - xl;
@@ -173,8 +162,14 @@ export const performClahe = (input: PlotBuffer, width: number, height: number, o
       const cdf22 = (hists[xr][yd][lum]);
 
       const newLum = (1 - fx) * (1 - fy) * cdf11 + (1 - fx) * fy * cdf12 + fx * (1 - fy) * cdf21 + fx * fy * cdf22;
-      const idx = (y * width + x);
-      output[idx] = newLum;
+      const idx = (y * width + x) * 4;
+
+      output[idx + 0] = newLum / 255;
+      output[idx + 1] = newLum / 255;
+      output[idx + 2] = newLum / 255;
+      output[idx + 3] = 1;
     }
   }
+  
+  return output;
 };
